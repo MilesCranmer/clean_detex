30c181578220a0e6701b2ff3186400feb60cfa33
MilesCranmer <miles.cranmer@gmail.com>
2019-08-19 22:01:37 -0400

    Clean up temporary files

--- clean_detex.sh
+++ clean_detex.sh
@@ -17,26 +17,26 @@ while (($# > 0)); do
 done
 
 echo "Backing up"
-cp $FILENAME .detex.tex
+cp $FILENAME /tmp/.$OUTPUTFILENAME.detex.tex
 echo "Converting inline tokens to X"
 
 if [ "$RAWTEX" -eq "0" ]; then
-    cat .detex.tex | ./vims '1,/\\begin{document}/d' > .detex1.5.tex
+    cat /tmp/.$OUTPUTFILENAME.detex.tex | ./vims '1,/\\begin{document}/d' > /tmp/.$OUTPUTFILENAME.detex1.5.tex
 else
-    cp .detex.tex .detex1.5.tex
+    cp /tmp/.$OUTPUTFILENAME.detex.tex /tmp/.$OUTPUTFILENAME.detex1.5.tex
 fi
 
-cat .detex.tex | ./vims  '%s/\\\[.\{-}\\\]/X/g' '%s/\$.\{-}\$/X/g' | ./vims '%s/\\cite{.\{-}}/X/g' '%s/\\citealt{.\{-}}/X/g' '%s/\\ref{.\{-}}/X/g' '%s/\\cref{.\{-}}/X/g' '%s/\\url{.\{-}}/X/g' | ./vims '%s/\\label{.\{-}}//g' '%g/\\begin{align}/,/\\end{align}/d' '%g/\\begin{align\*}/,/\\end{align\*}/d' '%g/\\begin{equation}/,/\\end{equation}/d' '%g/\\begin{equation\*}/,/\\end{equation\*}/d' > .detex2.tex
+cat /tmp/.$OUTPUTFILENAME.detex.tex | ./vims  '%s/\\\[.\{-}\\\]/X/g' '%s/\$.\{-}\$/X/g' | ./vims '%s/\\cite{.\{-}}/X/g' '%s/\\citealt{.\{-}}/X/g' '%s/\\ref{.\{-}}/X/g' '%s/\\cref{.\{-}}/X/g' '%s/\\url{.\{-}}/X/g' | ./vims '%s/\\label{.\{-}}//g' '%g/\\begin{align}/,/\\end{align}/d' '%g/\\begin{align\*}/,/\\end{align\*}/d' '%g/\\begin{equation}/,/\\end{equation}/d' '%g/\\begin{equation\*}/,/\\end{equation\*}/d' > /tmp/.$OUTPUTFILENAME.detex2.tex
 
 
 
 echo "Running detex"
-detex .detex2.tex > .detex3.tex
+detex /tmp/.$OUTPUTFILENAME.detex2.tex > /tmp/.$OUTPUTFILENAME.detex3.tex
 echo "Cleaning detex"
-cat .detex3.tex | ./vims ':%g/.*\S.*\n.*\S.*\n/.,/\n\s*\n/j' > .detex4.tex
+cat /tmp/.$OUTPUTFILENAME.detex3.tex | ./vims ':%g/.*\S.*\n.*\S.*\n/.,/\n\s*\n/j' > /tmp/.$OUTPUTFILENAME.detex4.tex
 for num in {1..10};
 do
-    cat .detex4.tex | ./vims ':%s/\n\s*\n\s*\n/\r\r/g' > .detex5.tex
-    cat .detex5.tex | ./vims ':%s/\n\s*\n\s*\n/\r\r/g' > .detex4.tex
+    cat /tmp/.$OUTPUTFILENAME.detex4.tex | ./vims ':%s/\n\s*\n\s*\n/\r\r/g' > /tmp/.$OUTPUTFILENAME.detex5.tex
+    cat /tmp/.$OUTPUTFILENAME.detex5.tex | ./vims ':%s/\n\s*\n\s*\n/\r\r/g' > /tmp/.$OUTPUTFILENAME.detex4.tex
 done
-cp .detex4.tex $OUTPUTFILENAME
+cp /tmp/.$OUTPUTFILENAME.detex4.tex $OUTPUTFILENAME
